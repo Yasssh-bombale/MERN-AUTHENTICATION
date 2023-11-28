777e9bdc4ecf97f6267f39ae5275a72951c382d4
Yash Vijay Bombale <yashbombale@gmail.com>
2023-11-28 21:52:30 +0530

    Handling backend and frontend together in signup component in client

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,31 +1,50 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 const Signup = () => {
+  const [formData, setFormData] = useState({});
+
+  const handleSubmit = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+  console.log(formData);
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const res = await axios.post("/api/auth/signup", formData);
+    console.log(res);
+  };
+
   return (
     <div className="max-w-lg mx-auto">
       <h1 className="text-3xl  text-center my-7">Sign Up</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className="flex flex-col gap-3">
           <input
             type="text"
             placeholder="username"
+            id="username"
             className="bg-slate-100 p-3 rounded-lg"
+            onChange={handleSubmit}
           />
           <input
             type="email"
             placeholder="email"
+            id="email"
             className="bg-slate-100 p-3 rounded-lg"
+            onChange={handleSubmit}
           />
           <input
             type="password"
             placeholder="password"
+            id="password"
             className="bg-slate-100 p-3 rounded-lg"
+            onChange={handleSubmit}
           />
           <button
             type="submit"
             className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-80"
           >
-            sign up
+            Sign up
           </button>
         </div>
         <div className="my-3 flex gap-3">
