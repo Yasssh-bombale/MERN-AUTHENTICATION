48e2a222b32a12d7f195c6248e991eebbe4962bf
Yash Vijay Bombale <yashbombale@gmail.com>
2023-11-28 08:58:45 +0530

    added routes and pages with header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { About, Home, Profile, Signin, Signup } from "./pages";
+import Header from "./components/Header";
+
 function App() {
-  return <h1>hellpow</h1>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/about"} element={<About />} />
+        <Route path={"/profile"} element={<Profile />} />
+        <Route path={"/signin"} element={<Signin />} />
+        <Route path={"/signup"} element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
