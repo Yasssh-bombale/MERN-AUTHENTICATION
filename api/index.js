4a4ecaab0f24e9c3af6671477c1f640357e6b936
Yash Vijay Bombale <yashbombale@gmail.com>
2023-11-28 12:34:22 +0530

    connected db to atlas cloud

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import { config } from "dotenv";
 
+config({
+  path: "config.env",
+});
 const app = express();
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log(`MONGO connected successfully`))
+  .catch((e) => console.log(`error while connecting to database ${e}`));
 
 app.listen(8000, () => {
-  console.log(`Server is successfully running on PORT:8000`);
+  console.log(`Server is successfully running on PORT:${process.env.PORT}`);
 });
