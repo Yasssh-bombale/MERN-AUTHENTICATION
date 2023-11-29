92311ad0c44803c1daf541015305f413e48ff6ad
Yash Vijay Bombale <yashbombale@gmail.com>
2023-11-29 22:51:29 +0530

    added redux-persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,6 +1,19 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+// FIXME: combineReducers is used to combine more than one reducers together;
+
+const rootReducer = combineReducers({ user: userReducer });
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-  reducer: { user: userReducer },
+  reducer: persistedReducer,
 });
 export default store;
+export const persistor = persistStore(store);
