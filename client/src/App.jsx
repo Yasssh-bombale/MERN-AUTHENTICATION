8bbc45f062032e06ee5a09570dd9c7b379e5121c
Yash Vijay Bombale <yashbombale@gmail.com>
2023-11-30 20:32:32 +0530

    make profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { About, Home, Profile, Signin, Signup } from "./pages";
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
@@ -9,9 +10,11 @@ function App() {
       <Routes>
         <Route path={"/"} element={<Home />} />
         <Route path={"/about"} element={<About />} />
-        <Route path={"/profile"} element={<Profile />} />
         <Route path={"/signin"} element={<Signin />} />
         <Route path={"/signup"} element={<Signup />} />
+        <Route element={<PrivateRoute />}>
+          <Route path={"/profile"} element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,22 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { useSelector } from "react-redux";
 const Header = () => {
+  const { currentUser } = useSelector((state) => state.user);
+  // console.log(currentUser.profilePicture);
   return (
-    <div className="bg-slate-200 p-4">
-      <div className="max-w-6xl flex justify-between items-center mx-auto">
+    <div className="bg-slate-200 p-4 box-border">
+      <div className="max-w-6xl flex justify-between items-center mx-auto ">
         <Link to={"/"}>
           <h1 className="font-bold text-xl">MERN Authentication</h1>
         </Link>
-        <ul className="flex gap-4">
+        <ul className="flex gap-4 items-center">
           <Link to={"/"}>
             <li>Home</li>
           </Link>
           <Link to={"/about"}>
             <li>About</li>
           </Link>
-          <Link to={"signin"}>
-            <li>SignIn</li>
+          <Link to={"/profile"}>
+            {currentUser ? (
+              <img
+                src={currentUser.profilePicture}
+                className="h-10 w-10 object-cover rounded-full"
+              />
+            ) : (
+              <li>Sign In</li>
+            )}
           </Link>
         </ul>
       </div>
