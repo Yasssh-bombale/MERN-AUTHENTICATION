01c8036562f8e8907e958fd8feaa95e2958a7b0f
Yash Vijay Bombale <yashbombale@gmail.com>
2023-12-03 12:28:33 +0530

    fixing bugs

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -8,14 +8,28 @@ import {
   uploadBytesResumable,
 } from "firebase/storage";
 import { app } from "../firebase";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import {
+  updateUserStart,
+  updateUserSuccess,
+  updateUserFailure,
+} from "../redux/user/userSlice";
+
 const Profile = () => {
   const [image, setImage] = useState(undefined);
-  const { currentUser } = useSelector((state) => state.user);
   const [imagePercent, setImagePercent] = useState(0);
   const [imageError, setImageError] = useState(false);
   const [formData, setFormData] = useState({});
+  const [updateSuccess, setUpdateSuccess] = useState(false);
   const fileRef = useRef(null);
-  console.log(imagePercent);
+  const { currentUser, loading, error } = useSelector((state) => state.user);
+
+  const dispatch = useDispatch();
+
+  // console.log(imagePercent);
+  // console.log(currentUser);
+  // console.log(currentUser.profilePicture);
   console.log(formData);
 
   const inputChnageHandler = (e) => {
@@ -102,7 +116,7 @@ const Profile = () => {
     <div className=" max-w-lg mx-auto gap-2 ">
       <h1 className="text-center text-3xl font-semibold mt-4">Profile</h1>
 
-      <form className="flex flex-col  mt-4 gap-4 ">
+      <form onSubmit={handleSubmit} className="flex flex-col  mt-4 gap-4 ">
         <input
           type="file"
           ref={fileRef}
@@ -143,6 +157,7 @@ const Profile = () => {
           id="username"
           placeholder="username"
           className="bg-slate-100 rounded-lg p-2 "
+          onChange={inputChnageHandler}
         />
         <input
           defaultValue={currentUser.email}
@@ -150,21 +165,29 @@ const Profile = () => {
           id="email"
           placeholder="email"
           className="bg-slate-100 rounded-lg p-2 "
+          onChange={inputChnageHandler}
         />
         <input
           type="password"
           id="password"
           placeholder="password"
           className="bg-slate-100 rounded-lg p-2 "
+          onChange={inputChnageHandler}
         />
         <button className="bg-slate-700 text-white w-full uppercase p-2 rounded-lg hover:opacity-95">
-          Update
+          {loading ? "Updating..." : "Update"}
         </button>
       </form>
       <div className="flex justify-between mt-3">
         <span className="text-red-600 cursor-pointer">Delete Account</span>
         <span className="text-red-600 cursor-pointer">Sign out</span>
       </div>
+      <p className="text-red-600 mt-3">
+        {error && "Opps ! Something went wrong "}
+      </p>
+      <p className="text-green-600 mt-5 ">
+        {updateSuccess && "User is updated successfully"}
+      </p>
     </div>
   );
 };
